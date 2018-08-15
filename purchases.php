40a9fb87df8654530ffaeff18f03757b0c3a272e
Jairo Perez <jairo.preztaveras@gmail.com>
2018-08-15 15:53:29 -0400

    Update the main title of the sales and purchases modules.

--- purchases.php
+++ purchases.php
@@ -17,7 +17,7 @@
 
         <!-- content -->
         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-          <h1 class=" text-center page-header">Control de Ventas</h1>
+          <h1 class=" text-center page-header">Control de Compras</h1>
           <h4 class="sub-header"><a class="pull-right" href="add_purchase.php"><i class="fa fa-plus" aria-hidden="true"> Añadir Compra</i></a></h4>
           <div class="table-responsive">
             <table class="table table-striped">
