e755628d86af1de3cd328beb8d38ee86fb22e801
Domingo Perez <domingoperez.23@gmail.com>
2018-08-15 23:18:21 -0700

    Add favicon icon

--- layout/header.php
+++ layout/header.php
@@ -8,7 +8,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 	<!-- Favicon -->
-	<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+	<link rel="shortcut icon" type="image/x-icon" href="../img/favicon-icon.ico">
 
 	<!-- Load css bootstrap -->
 	<link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
